c95ae41610b7f932553bf853142c59116deeaaf3
Tran Nam Hai <hai.tn@gianty.com.vn>
2024-11-04 11:42:30 +0700

    Add project files.

---
+++ gitlab-runner-cicd/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using gitlab_runner_cicd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace gitlab_runner_cicd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
